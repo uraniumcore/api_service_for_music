81c80b99f61f4ab5e669d79986ee09b36a3352c0
Arsen <arsenabdizhamiyev00@gmail.com>
2025-11-09 22:40:08 +0500

    api /info/{filename}

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -15,9 +16,35 @@ func main() {
 	
 	r := mux.NewRouter()
 	r.HandleFunc("/audio/{filename}", serveAudio).Methods("GET")
+	r.HandleFunc("/info/{filename}", infoHandler).Methods("GET")
 
 	fmt.Println("🚀 Server running on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+// infoHandler writes the name and extension of a file (if it exists) in JSON
+func infoHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	filename := vars["filename"]
+
+	// List of extensions we support in the audio folder
+	exts := []string{".m4a", ".mp3"}
+
+	for _, ext := range exts {
+		fp := filepath.Join("audio", filename+ext)
+		if _, err := os.Stat(fp); err == nil {
+			// Found the file; return JSON with name and extension (without dot)
+			resp := map[string]string{
+				"name":      filename,
+				"extension": ext[1:],
+			}
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(resp)
+			return
+		}
+	}
+
+	http.Error(w, "File not found", http.StatusNotFound)
 }
 
 // serveAudio serves a local .m4a file
